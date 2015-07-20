be8904f34b95747e0a592a8030a2c3cf95613361
Adrriver <Adrriver@nmsu.edu>
2015-07-20 12:01:00 -0600

    Initial GUI construction

--- src/com/adrianmrivera/FXMLDocumentController.java
+++ src/com/adrianmrivera/FXMLDocumentController.java
@@ -26,9 +26,10 @@ public class FXMLDocumentController implements Initializable {
     private ToolBar prToolbar;
     private ToolBar secToolbar;
     private TabPane tabPane;
-    private ScrollPane dataPane;
+    private ScrollPane dataPane = new ScrollPane();
     private ScrollPane varPane;
     private HBox tTipHBox;
+    
     
     
     @FXML
--- src/com/adrianmrivera/GraduateStats.java
+++ src/com/adrianmrivera/GraduateStats.java
@@ -10,6 +10,7 @@ import javafx.embed.swing.*;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 import javax.swing.JTable;
 import javax.swing.SwingUtilities;
@@ -18,9 +19,12 @@ import javax.swing.table.*;
 /**
  *
  * @author Adrian_and_Alanna
- */
+ */ 
+     
 public class GraduateStats extends Application {
-    
+    //fields
+    private ScrollPane dataPane = new ScrollPane();  
+   
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
@@ -34,10 +38,11 @@ public class GraduateStats extends Application {
     
          final SwingNode swingNode = new SwingNode();
 
-        createSwingContent(swingNode);
-       
-        
-        
+            createSwingContent(swingNode);
+            
+                                    
+            dataPane.setContent(swingNode);
+            
     }
 
     /**
@@ -49,7 +54,33 @@ public class GraduateStats extends Application {
 
     private void createSwingContent(final SwingNode swingNode) {
         
-                
+        TableModel dataModel;
+       
+        dataModel = new AbstractTableModel(){
+            @Override
+            public int getColumnCount(){
+                return 25;
+            }
+            @Override
+            public int getRowCount(){
+                return 200;
+            }
+            
+            @Override
+            public Object getValueAt(int row, int col) {
+                return col*row;
+            }
+            public Object getValueAt(double row, double col){
+                return (Double)(row*col);
+            }
+            
+        };
+        
+            JTable dataTable = new JTable(dataModel);
+        
+                SwingUtilities.invokeLater(() -> {
+                   swingNode.setContent(dataTable); 
+                });
         
     }
     
